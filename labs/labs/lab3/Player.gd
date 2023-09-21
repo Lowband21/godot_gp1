6dc4394660efc87ab6ec222d2e0acdaec665a0dc
Lowband <GraysonHieb21@Gmail.com>
2023-09-21 09:32:48 -0600

    lab3

--- labs/labs/lab3/Player.gd
+++ labs/labs/lab3/Player.gd
@@ -1,28 +1,46 @@
 extends RigidBody2D
 
-var speed = 500
+# Constants and Variables
+var speed = 800
+var jump_strength = 2000
+var air_control = 0.5  # Between 0 and 1, where 1 is full control in the air, and 0 is no control.
+var jump_count = 0
+var max_jumps = 2  # Allows for double jump
+var grounded_threshold = 0  # Linear velocity y threshold for considering the player as grounded
+
+# Input
 var movement_input = 0
 
+# Returns true if the character is considered grounded.
+func is_grounded():
+	return $GroundCheck.is_colliding()
+
+func _ready():
+	self.gravity_scale = 6  # Adjust as necessary for the desired gravity effect
+
 func _process(delta):
-	movement_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	movement_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 
-	# Update animation
-	if movement_input != 0:
-		$AnimatedSprite2D.play("run")
+	# Animation and flipping
+	if abs(movement_input) > 0:
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = movement_input < 0
 	else:
-		$AnimatedSprite2D.play("idle")
+		$AnimatedSprite2D.play("up")
+		
 
-	# Flip character
-	if movement_input > 0:
-		$AnimatedSprite2D.flip_h = false
-	elif movement_input < 0:
-		$AnimatedSprite2D.flip_h = true
 	
-func _integrate_forces(state):
-	var applied_velocity = Vector2()
-	applied_velocity.x = movement_input * speed
-	state.linear_velocity = applied_velocity
+func _physics_process(delta):
+	# Movement
+	var control_factor = 1 if is_grounded() else air_control
+	var target_velocity = Vector2(movement_input * speed, linear_velocity.y)
+	linear_velocity = linear_velocity.lerp(target_velocity, control_factor)
 
-func _on_body_entered(body):  # optional, if you need collision logic
-	# Handle collision logic here
-	pass
+	# Jumping logic
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
+		linear_velocity.y = -jump_strength  # Initial velocity for jump
+		jump_count += 1
+
+	if is_grounded() and linear_velocity.y > -1:  # Add a small threshold for y velocity to ensure character is descending or at rest.
+		jump_count = 0  # Reset jump count when grounded
+
